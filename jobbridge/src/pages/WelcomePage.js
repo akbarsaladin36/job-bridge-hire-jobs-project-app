d9d1f39a58c48537f99d6275c93246610de98add
akbarsaladin36 <asong36@gmail.com>
2021-05-16 00:35:20 +0700

    added welcome page and need fix this welcome page

---
+++ jobbridge/src/pages/WelcomePage.js
@@ -0,0 +1,56 @@
+import React, { Component } from "react";
+import { Row, Col, Button, Container } from "react-bootstrap";
+// import { Link } from "react-router-dom";
+import WelcomePageStyle from "./WelcomePageStyle.module.css";
+
+class WelcomePage extends Component {
+  render() {
+    return (
+      <>
+        <Container fluid>
+          <Row>
+            <Col lg={7} className={WelcomePageStyle.left_background}>
+              <img
+                src="/img/left-column-image.jpg"
+                className={WelcomePageStyle.image_background}
+                alt="job bridge background"
+              />
+              <img
+                src="/img/peword-white-logo.png"
+                className={WelcomePageStyle.job_bridge_brand}
+                alt="job bridge brand"
+              />
+              <div>
+                <h1 className={WelcomePageStyle.job_bridge_brand_word}>
+                  Temukan developer berbakat dan terbaik di berbagai bidang
+                  keahlian
+                </h1>
+              </div>
+              <div className={WelcomePageStyle.button_position}>
+                <Button className={`${WelcomePageStyle.sign_in_button} mt-2`}>
+                  <b>Masuk sebagai pekerja</b>
+                </Button>
+                <p className={WelcomePageStyle.left_column_small_word}>Atau</p>
+                <Button className={`${WelcomePageStyle.sign_in_button_2} mt-2`}>
+                  <b>Masuk sebagai perekrut</b>
+                </Button>
+              </div>
+            </Col>
+            <Col
+              lg={4}
+              className={`${WelcomePageStyle.right_column} mt-5 pt-5`}
+            >
+              <h2>Halo Pewpeople</h2>
+              <p className="mt-3">
+                lorem ipsum dolor sit amet, consectetur adipiscing elit. In
+                euismod ipsum et dui rhoncus auctor
+              </p>
+            </Col>
+          </Row>
+        </Container>
+      </>
+    );
+  }
+}
+
+export default WelcomePage;
