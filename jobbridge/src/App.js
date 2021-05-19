ad13efad6008700028d7467493c9bbf88963c514
elazsyahranie <elazaribrahims@gmail.com>
2021-05-19 18:37:53 +0700

    Profile Company

--- jobbridge/src/App.js
+++ jobbridge/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import ProfilePage from "./pages/profile/profile";
 
 class App extends Component {
   render() {
@@ -9,6 +10,7 @@ class App extends Component {
         <Switch>
           {/* sesuaikan dengan codingan kemaren untuk route */}
           <Route path="/" exact component={Home} />
+          <Route path="/profilepage" exact component={ProfilePage} />
         </Switch>
       </Router>
     );
