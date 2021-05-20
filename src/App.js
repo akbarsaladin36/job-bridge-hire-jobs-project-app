0a30124280be72ea83433c19a59269c914c2f75c
akbarsaladin36 <asong36@gmail.com>
2021-05-21 00:16:50 +0700

    updated search worker page and added responsive page

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import LoginPage from "./pages/auth/worker/Login/Login";
 import RegisterPage from "./pages/auth/worker/Register/Register";
 import ResetPasswordPage from "./pages/auth/reset_password/ResetPassword/ResetPassword";
 import ChangePasswordPage from "./pages/auth/reset_password/ChangePassword/ChangePassword";
-import WelcomePage from "./pages/WelcomePage";
 import LoginAfterResetPasswordPage from "./pages/auth/reset_password/LoginAfterResetPassword/LoginAfterResetPassword";
 import LoginRecruiterPage from "./pages/auth/recruiter/Login/Login_Recruiter";
 import RegisterRecruiterPage from "./pages/auth/recruiter/Register/Register_Recruiter";
@@ -16,7 +15,6 @@ class App extends Component {
       <Router>
         <Switch>
           {/* sesuaikan dengan codingan kemaren untuk route */}
-          <Route path="/" exact component={WelcomePage} />
           <Route path="/auth/worker/login" exact component={LoginPage} />
           <Route path="/auth/worker/register" exact component={RegisterPage} />
           <Route
