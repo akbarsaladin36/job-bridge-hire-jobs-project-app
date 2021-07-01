08de10d5f9119e3f37698aad5be2f28100622b8b
Timotius-Nugroho <timotiusnugroho999@gmail.com>
2021-07-01 22:12:11 +0700

    makan bang !

--- src/redux/reducer/auth.js
+++ src/redux/reducer/auth.js
@@ -122,6 +122,34 @@ const auth = (state = initialState, action) => {
         data: action.payload.data.data,
         msg: action.payload.data.msg,
       };
+    case "GET_RECRUITER_PENDING": // prosesnya sedang berjalan
+      return {
+        ...state,
+        login: false,
+        roleUser: 1,
+        isLoading: true,
+        isError: false,
+        msg: "",
+      };
+    case "GET_RECRUITER_FULFILLED": // ketika sukses
+      return {
+        ...state,
+        login: true,
+        roleUser: 1,
+        isLoading: false,
+        isError: false,
+        data: action.payload.data.data[0],
+        msg: action.payload.data.msg,
+      };
+    case "GET_RECRUITER_REJECTED": // ketika gagal
+      return {
+        ...state,
+        login: false,
+        roleUser: 1,
+        isLoading: false,
+        isError: true,
+        msg: action.payload.response.data.msg,
+      };
     case "REGISTER_WORKER_REJECTED":
       return {
         ...state,
